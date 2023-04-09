1279cafda17c39a24b52f5d3d8ebb6c3ad4b7103
forckes <forckes@proton.me>
2023-04-09 14:01:52 +0300

    init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Box } from "../Box/Box";
+
+class App extends Component {
+	state = {};
+
+	render() {
+		return <Box></Box>;
+	}
+}
+
+export default App;
