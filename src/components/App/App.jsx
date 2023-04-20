2fc1c6a97a7c111d0ddffaa7b14690b42630ab4d
forckes <forckes@proton.me>
2023-04-20 19:30:37 +0300

    Add svg icons

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,25 @@ import Contacts from "components/Contacts/Contacts";
 
 class App extends Component {
 	state = {
-		contacts: [
-			{ id: "id-1", contactName: "Rosie Simpson", tel: "459-12-56" },
-			{ id: "id-2", contactName: "Hermione Kline", tel: "443-89-12" },
-			{ id: "id-3", contactName: "Eden Clements", tel: "645-17-79" },
-			{ id: "id-4", contactName: "Annie Copeland", tel: "227-91-26" }
-		],
+		contacts: [],
 		filter: ""
 	};
+
+	componentDidMount() {
+		const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
+		if (parsedContacts) {
+			this.setState({ contacts: parsedContacts });
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		const actualContacts = this.state.contacts;
+		const prevContacts = prevState.contacts;
+
+		if (actualContacts !== prevContacts) {
+			localStorage.setItem("contacts", JSON.stringify(actualContacts));
+		}
+	}
 
 	changeFilter = e => {
 		this.setState({ filter: e.currentTarget.value });
