d28746bb79c8ff8aad9d3ff3ab18e32432e1ed35
forckes <forckes@proton.me>
2023-04-28 18:07:25 +0300

    rewrite a project on hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,37 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useLocalStorage } from "hooks/useLocalStorage";
 import shortid from "shortid";
 import { Box } from "../Box/Box";
 import Title from "components/Title/Title";
 import PhoneForm from "components/PhoneForm/PhoneForm";
 import Contacts from "components/Contacts/Contacts";
 
-class App extends Component {
-	state = {
-		contacts: [],
-		filter: ""
+function App() {
+	const [contacts, setContacts] = useLocalStorage("contacts", []);
+	const [filter, setFilter] = useState("");
+
+	const changeFilter = e => {
+		setFilter(e.currentTarget.value);
 	};
-
-	componentDidMount() {
-		const parsedContacts = JSON.parse(localStorage.getItem("contacts"));
-		if (parsedContacts) {
-			this.setState({ contacts: parsedContacts });
-		}
-	}
-
-	componentDidUpdate(prevProps, prevState) {
-		const actualContacts = this.state.contacts;
-		const prevContacts = prevState.contacts;
-
-		if (actualContacts !== prevContacts) {
-			localStorage.setItem("contacts", JSON.stringify(actualContacts));
-		}
-	}
-
-	changeFilter = e => {
-		this.setState({ filter: e.currentTarget.value });
-	};
-	createContact = (contactName, tel) => {
-		const isContactExists = this.state.contacts.some(
+	const createContact = (contactName, tel) => {
+		const isContactExists = contacts.some(
 			contact => contact.contactName === contactName
 		);
 		const contact = {
@@ -43,43 +26,39 @@ class App extends Component {
 			alert(`${contactName} is already in contacts`);
 			return;
 		}
-		this.setState(prevState => ({
-			contacts: [contact, ...prevState.contacts]
-		}));
+
+		setContacts(prevState => [contact, ...prevState]);
 	};
-	handleDelete = contactId => {
-		this.setState(prevState => ({
-			contacts: prevState.contacts.filter(contact => contact.id !== contactId)
-		}));
+	const handleDelete = contactId => {
+		setContacts(prevState =>
+			prevState.filter(contact => contact.id !== contactId)
+		);
 	};
 
-	render() {
-		const { contacts, filter } = this.state;
-		const normalizedFilter = filter.toLowerCase();
-		const visibleContacts = contacts.filter(contact =>
-			contact.contactName.toLowerCase().includes(normalizedFilter)
-		);
+	const normalizedFilter = filter.toLowerCase();
+	const visibleContacts = contacts.filter(contact =>
+		contact.contactName.toLowerCase().includes(normalizedFilter)
+	);
 
-		return (
-			<Box
-				display='flex'
-				alignItems='center'
-				justifyContent='center'
-				flexDirection='column'
-				margin={4}
-			>
-				<Title text={"Phonebook"} />
-				<PhoneForm onCreateContact={this.createContact} />
-				<Title text={"Contacts"} />
-				<Contacts
-					onChange={this.changeFilter}
-					value={filter}
-					contacts={visibleContacts}
-					onDelete={this.handleDelete}
-				/>
-			</Box>
-		);
-	}
+	return (
+		<Box
+			display='flex'
+			alignItems='center'
+			justifyContent='center'
+			flexDirection='column'
+			margin={4}
+		>
+			<Title text={"Phonebook"} />
+			<PhoneForm onCreateContact={createContact} />
+			<Title text={"Contacts"} />
+			<Contacts
+				onChange={changeFilter}
+				value={filter}
+				contacts={visibleContacts}
+				onDelete={handleDelete}
+			/>
+		</Box>
+	);
 }
 
 export default App;
--- src/components/PhoneForm/PhoneForm.jsx
+++ src/components/PhoneForm/PhoneForm.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Box } from "components/Box/Box";
-import { Formik, Form, Field } from "formik";
+import { useForm } from "react-hook-form";
 import { Label } from "./PhoneForm.styled";
 import IconButton from "components/IconButton/IconButton";
 import { ReactComponent as AddIcon } from "../../icons/user-add-svgrepo-com.svg";
@@ -26,68 +26,62 @@ export const Button = styled(IconButton)`
 	}
 `;
 
-const StyledForm = styled(Form)`
+const StyledForm = styled.form`
 	display: flex;
 	justify-content: center;
 	flex-direction: column;
 	align-items: center;
 	gap: 10px;
 `;
-const Input = styled(Field)`
+const Input = styled.input`
 	border: 1px solid #71717171;
 	border-radius: 4px;
 	height: 26px;
 	font-size: 22px;
 `;
 
-class PhoneForm extends Component {
-	state = {
-		name: "",
-		number: ""
+export default function PhoneForm({ onCreateContact }) {
+	const { register, handleSubmit, resetField } = useForm();
+
+	const onHandleSubmit = ({ name, number }) => {
+		onCreateContact(name, number);
+		resetField("name");
+		resetField("number");
 	};
 
-	handleSubmit = (values, { resetForm }) => {
-		this.props.onCreateContact(values.name, values.number);
-		resetForm();
-	};
-	render() {
-		return (
-			<Box
-				display='flex'
-				alignItems='center'
-				justifyContent='center'
-				flexDirection='column'
-			>
-				<Formik onSubmit={this.handleSubmit} initialValues={this.state}>
-					<StyledForm>
-						<Label htmlFor='name'>
-							Name
-							<Input
-								type='text'
-								name='name'
-								pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-								title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-								required
-							/>
-						</Label>
-						<Label htmlFor='number'>
-							Number
-							<Input
-								type='tel'
-								name='number'
-								pattern='\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
-								title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-								required
-							/>
-						</Label>
-						<Button type='submit'>
-							Add contact
-							<AddIcon width='40px' fill='#fff' height='40px' />
-						</Button>
-					</StyledForm>
-				</Formik>
-			</Box>
-		);
-	}
+	return (
+		<Box
+			display='flex'
+			alignItems='center'
+			justifyContent='center'
+			flexDirection='column'
+		>
+			<StyledForm onSubmit={handleSubmit(onHandleSubmit)}>
+				<Label htmlFor='name'>
+					Name
+					<Input
+						type='text'
+						{...register("name")}
+						pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+						title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+						required
+					/>
+				</Label>
+				<Label htmlFor='number'>
+					Number
+					<Input
+						type='tel'
+						{...register("number")}
+						pattern='\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
+						title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+						required
+					/>
+				</Label>
+				<Button type='submit'>
+					Add contact
+					<AddIcon width='40px' fill='#fff' height='40px' />
+				</Button>
+			</StyledForm>
+		</Box>
+	);
 }
-export default PhoneForm;
